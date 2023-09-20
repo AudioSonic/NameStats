5bbeb2f93e024b3e5562092dfcb4718b295f3204
AudioSonic <79091924+AudioSonic@users.noreply.github.com>
2023-09-20 12:50:33 +0200

    Dropdown Menü
    Das Dropdown Menü mit den Staaten wurde hinzugefügt. Es wird mit Hilfe eines Wörterbuchs geschaut welches Kürzel zu welchem Staat gehört um die Information dann zu verarbeiten

--- NameStats.py
+++ NameStats.py
@@ -3,58 +3,132 @@ import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import *
 
+staaten_liste = ["Alabama", "Alaska", "Arizona", "Arkansas", "California",
+    "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+    "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+    "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+    "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+    "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+    "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+    "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+    "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+    "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+
+staaten_woerterbuch = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY"
+}
+
+
+
+
 def eingabe():
     username = name.get()
-    #state = state.get()
+    state = staat.get()
     #gender = gender.get()
     #Year = year.get() 
-    
+    anzahl = 0
+
     xx = []
     xy = []
     
-    anzahl = 0
+
 
     with open("names.csv", "r") as file:  
+        #Wandelt die Kürzel der csv Datei mit Hilfe von staaten_woerterbuch um
+        if state in staaten_woerterbuch:
+            Kuerzel = staaten_woerterbuch[state]
+        else:
+            print(state)   
+        
         for line in file:
             split = line.strip().split(",")
-            if split[1] == username:
-             xx.append(split[2])
-             xy.append(split[5])
-             anzahl += 1  # Erhöhen Sie die Anzahl für jede Zeile
+            
 
-    print("Die Anzahl des Namens " + username + " ist:", anzahl)
+            #Die Koordinaten werden erstellt
+            if split[1] == username and split[4] == Kuerzel:
+             #xx.append(split[2])
+             #xy.append(split[5])
+    
+             anzahl += 1
+    print(anzahl)
+                
 
-    plt.plot(xx, xy)
-    plt.show()
-
+#Das GUI 
 root = tk.Tk()
 root.geometry("800x500")
 root.title("NameStats")
 
+#Die Überschrift
 label = tk.Label(root, text="NameStats", font=('Arial', 18))
 label.pack(padx=20, pady=20)
 
+#Das Textfeld für den Namen
 name = tk.Entry(root)
 name.pack()
 name.focus()
 
-# You had "Button" instead of "tk.Button" for creating the button widget.
-b = tk.Button(root, text="Submit", command=eingabe)
-b.pack()
+#Das "States" Dropdown Menü. staat bestimmt die Standardoption
+staat = tk.StringVar(root)
+staat.set(staaten_liste[0])
+
+dropdown = tk.OptionMenu(root, staat, *staaten_liste)
+dropdown.pack()
+
+
+#Der Confirm Button. Er soll die Eingabe nur genehmigen, wenn alles weitere funktionert
+button = tk.Button(root, text="Submit", command=eingabe)
+button.pack()
 
 root.mainloop()
 
-
-"""
-with open("../names.csv", "r") as file:   
-    
-    
-    for line in file:
-        split = line.strip().split(",") 
-        if name == split[1]: 
-            print(split[1], split[2], split[3], split[4], split[5])
-            break
-
-"""
 
 
